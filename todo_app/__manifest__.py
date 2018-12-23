477f27fc1068699c5e16ca3b428527ac91ee4c45
Po Lynn <polynn@global-connect.asia>
2018-12-24 00:01:46 +0630

    todo

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'Todo Application',
+    'description':'Manage personal to do tasks',
+    'author':'R&D Team',
+    'depends':['base'],
+    'application':True,
+    'data':[
+        'views/todo_menu.xml',
+        'views/todo_view.xml',
+        'views/res_partner_view.xml'
+    ]
+}
---
+++ todo_app/models/todo_task_model.py
@@ -0,0 +1,24 @@
+from odoo import fields,api, models
+
+class Todotask(models.Model):
+    _name = 'todo.task'
+    _description = 'Todo Task'
+    name = fields.Char('Description',require=True)
+    is_done = fields.Boolean('Done')
+    active = fields.Boolean('Active?',default=True)
+    date_deadline = fields.Date('Deadline')
+    user_id = fields.Many2one('res.users',string='Responsible',default=lambda self:self.env.user)
+    team_ids = fields.Many2many('res.partner',string='Team')
+    
+    @api.multi
+    def do_clear_done(self):
+        for task in self:
+            task.active = False
+
+        return True
+
+    @api.multi
+    def write(self,values):
+        if 'active' not in values:
+            values['active']=True
+        super().write(values)
